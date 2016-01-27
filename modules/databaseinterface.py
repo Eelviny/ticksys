66430fd65493d61777935642b95faebaa5237e6a
Elvin Luff <elvinluff@gmail.com>
2016-01-27 09:38:36 +0000

    Database creation implemented

--- modules/databaseinterface.py
+++ modules/databaseinterface.py
@@ -8,15 +8,29 @@ class Database():
 		self.conn = sqlite.connect(dbname)
 		self.c = self.conn.cursor()
 		
-	def execute(statement):
+	def execute(self, statement):
 		self.c.execute(statement)
 		
-	def commit():
+	def commit(self):
 		self.conn.commit()
 		
-	def close():
+	def close(self):
 		self.conn.close()
 		del self
 		
-def newDB():
-	#TODO: Create a new database that fits the database schema exactly
+	def write(self, table, values):
+		if table == "userinfo":
+			self.execute('INSERT INTO userinfo VALUES (?,?,?,?)', values)
+		if table == "ticket_types":
+			self.execute('INSERT INTO ticket_types VALUES (?,?,?,?)', values)
+		if table == "orders":
+			self.execute('INSERT INTO orders VALUES (?,?,?,?)', values)
+		
+		
+def newDB(path):
+	newdb = Database(path)
+	newdb.execute('CREATE TABLE ticket_types (ID INT PRIMARY KEY NOT NULL, tName TEXT NOT NULL, tPrice INT NOT NULL, tInfo TEXT')
+	newdb.execute('CREATE TABLE userinfo (ID INT PRIMARY KEY NOT NULL, fName TEXT NOT NULL, lName TEXT, randomID CHAR(10) NOT NULL')
+	newdb.execute('CREATE TABLE orders (ID INT PRIMARY KEY NOT NULL, Quantity INT NOT NULL, userID INT NOT NULL, ticketTypeID INT NOT NULL')
+	newdb.commit()
+	newdb.close()
