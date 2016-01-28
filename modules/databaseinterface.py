858612d12dfc63bcd7e7d2445351467862c6e569
Elvin Luff <elvinluff@gmail.com>
2016-01-28 14:55:38 +0000

    Add more database statements

--- modules/databaseinterface.py
+++ modules/databaseinterface.py
@@ -5,7 +5,7 @@ import sqlite3
 # TODO: Link file access to this
 class Database():
 	def __init__(self, dbname):
-		self.conn = sqlite.connect(dbname)
+		self.conn = sqlite3.connect(dbname)
 		self.c = self.conn.cursor()
 		
 	def execute(self, statement):
@@ -18,19 +18,30 @@ class Database():
 		self.conn.close()
 		del self
 		
+	def read(self, table, query="")
+		if query == "":
+			self.execute('SELECT * FROM ?', table)
+		else:
+			query = [table, query]
+			self.execute('SELECT * FROM ? WHERE ?', query)
+		return self.c.fetchall()
+		
 	def write(self, table, values):
-		if table == "userinfo":
-			self.execute('INSERT INTO userinfo VALUES (?,?,?,?)', values)
-		if table == "ticket_types":
-			self.execute('INSERT INTO ticket_types VALUES (?,?,?,?)', values)
-		if table == "orders":
-			self.execute('INSERT INTO orders VALUES (?,?,?,?)', values)
+		if table == 'ticket_types':
+			self.execute('INSERT INTO ticket_types (tName, tPrice, tInfo) VALUES (?,?,?)', values)
+		if table == 'userinfo':
+			self.execute('INSERT INTO userinfo (fName, lName, randomID) VALUES (?,?,?)', values)
+		if table == 'orders':
+			self.execute('INSERT INTO orders (quantity, userID, ticketTypeID) VALUES (?,?,?)', values)
+			
+	def nextAvail(self)
+		self.c.lastrowid
 		
 		
 def newDB(path):
 	newdb = Database(path)
-	newdb.execute('CREATE TABLE ticket_types (ID INT PRIMARY KEY NOT NULL, tName TEXT NOT NULL, tPrice INT NOT NULL, tInfo TEXT')
-	newdb.execute('CREATE TABLE userinfo (ID INT PRIMARY KEY NOT NULL, fName TEXT NOT NULL, lName TEXT, randomID CHAR(10) NOT NULL')
-	newdb.execute('CREATE TABLE orders (ID INT PRIMARY KEY NOT NULL, Quantity INT NOT NULL, userID INT NOT NULL, ticketTypeID INT NOT NULL')
+	newdb.execute('CREATE TABLE ticket_types (ID INTEGER PRIMARY KEY AUTOINCREMENT, tName TEXT, tPrice INTEGER, tInfo TEXT);')
+	newdb.execute('CREATE TABLE userinfo (ID INT PRIMARY KEY AUTOINCREMENT, fName TEXT, lName TEXT, randomID CHAR(10));')
+	newdb.execute('CREATE TABLE orders (ID INT PRIMARY KEY AUTOINCREMENT, quantity INTEGER, userID INTEGER, ticketTypeID INTEGER);')
 	newdb.commit()
 	newdb.close()
