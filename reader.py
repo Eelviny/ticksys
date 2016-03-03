d968986013c5bb0e9c048233f371d4cc7f54d065
Elvin Luff <elvinluff@gmail.com>
2016-03-03 14:15:05 +0000

    All buttons linked to events

--- reader.py
+++ reader.py
@@ -1,20 +1,98 @@
+#!/usr/bin/env python3
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Reader():
 	def __init__(self):
-		builder = Gtk.Builder()
-		builder.add_from_file("reader.glade")
-		builder.connect_signals(self)
-		window = builder.get_object("window1")
-		window.show_all()
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file("reader.glade")
+		self.builder.connect_signals(self)
+		self.window = self.builder.get_object("window1")
+		self.window.show_all()
+		
+	def textSet(self, text):
+		entry = self.builder.get_object("entry1")
+		entry.set_text(text)
+		
+	def textGet(self):
+		entry = self.builder.get_object("entry1")
+		return entry.get_text()
+		
+	def textAdd(self, text):
+		self.textSet(self.textGet() + text)
 		
 	def onDeleteWindow(self, *args):
-			Gtk.main_quit(*args)
+		Gtk.main_quit(*args)
+		
+	def entry1_icon_press_cb(self, i1, i2, i3):
+		self.textSet("")
+		print(i1, i2, i3)
 
-	def onButtonPressed(self, button):
-    		print("Hello World!")
+	def button1_clicked_cb(self, button):
+		self.textAdd("0")
+		print(button)
+		
+	def button2_clicked_cb(self, button):
+		self.textAdd("1")
+		print(button)
+		
+	def button3_clicked_cb(self, button):
+		self.textAdd("2")
+		print(button)
+		
+	def button4_clicked_cb(self, button):
+		self.textAdd("3")
+		print(button)
+		
+	def button5_clicked_cb(self, button):
+		self.textAdd("4")
+		print(button)
+		
+	def button6_clicked_cb(self, button):
+		self.textAdd("5")
+		print(button)
+		
+	def button7_clicked_cb(self, button):
+		self.textAdd("6")
+		print(button)
+		
+	def button8_clicked_cb(self, button):
+		self.textAdd("7")
+		print(button)
+		
+	def button9_clicked_cb(self, button):
+		self.textAdd("8")
+		print(button)
+		
+	def button10_clicked_cb(self, button):
+		self.textAdd("9")
+		print(button)
+		
+	def button11_clicked_cb(self, button):
+		self.textAdd("A")
+		print(button)
+		
+	def button12_clicked_cb(self, button):
+		self.textAdd("B")
+		print(button)
+		
+	def button13_clicked_cb(self, button):
+		self.textAdd("C")
+		print(button)
+		
+	def button14_clicked_cb(self, button):
+		self.textAdd("D")
+		print(button)
+		
+	def button15_clicked_cb(self, button):
+		self.textAdd("E")
+		print(button)
+		
+	def button16_clicked_cb(self, button):
+		self.textAdd("F")
+		print(button)
 
 main = Reader()
 Gtk.main()
