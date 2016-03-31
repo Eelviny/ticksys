837f94eb8ae22abed7fe8833dda5a62bfa6f8c98
Eelviny <elvinluff@gmail.com>
2016-03-31 17:24:11 +0100

    Fileaccess module now creates dialog boxes

--- fileaccess.py
+++ fileaccess.py
@@ -2,17 +2,20 @@
 
 # File Access Module
 
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 from dbinterface import *
 
 def setFile(path):
 	# Validate that it is a database file if the last 3 characters are .db
 	if path[-3:] != '.db':
-		raise NameError
+		raise TypeError
 	# Try opening the database. Remember to catch exceptions
 	opendb = Database(path)
 	# Verify that the database is for this program
 	if opendb.verify() == False:
-		raise NameError
+		raise TypeError
 	# Only create a new entry if it's not there - no duplicates
 	if opendb not in dbrunning:
 		dbrunning.append(opendb)
@@ -24,3 +27,25 @@ def newFile(path):
 	if path[-3:] != ".db":
 		path += ".db"
 	return newDB(filestring)
+	
+def openDialog(parent=None):
+	dialog = Gtk.FileChooserDialog("Please choose a file", parent, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+	response = dialog.run()
+	if response == Gtk.ResponseType.OK:
+		print("Open clicked")
+		print("File selected: " + dialog.get_filename())
+	elif response == Gtk.ResponseType.CANCEL:
+		print("Cancel clicked")
+	dialog.destroy()
+
+def saveDialog(parent=None):
+	dialog = Gtk.FileChooserDialog("Please choose a location", parent, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+	response = dialog.run()
+	if response == Gtk.ResponseType.OK:
+		print("File selected: " + dialog.get_filename())
+		response = dialog.get_filename()
+	elif response == Gtk.ResponseType.CANCEL:
+		print("Cancel clicked")
+		response = None
+	dialog.destroy()
+	return response
