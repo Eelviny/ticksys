63dbfbb4228b12545cdb6c71262379b874b2c34f
Eelviny <elvinluff@gmail.com>
2016-04-02 00:16:43 +0100

    Start to add support for Save As - it's going to be complicated!

--- fileaccess.py
+++ fileaccess.py
@@ -6,6 +6,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from dbinterface import *
+#from shutil import copyfile
 
 def setFile(path):
 	# Validate that it is a database file if the last 3 characters are .db
@@ -25,7 +26,7 @@ def newFile(path):
 	# Make sure the new file has the correct ending
 	if path[-3:] != ".db":
 		path += ".db"
-	return newDB(filestring)
+	return path
 
 # Create a dialog box for selecting the sqlite file
 def openDialog(parent=None):
@@ -53,18 +54,30 @@ def openDialog(parent=None):
 	# Return the database object
 	return response
 
-def newDialog(parent=None):
-	dialog = Gtk.FileChooserDialog("Please choose a file", parent, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+def saveDialog(parent=None, db=None):
+	# Create the dialog object
+	dialog = Gtk.FileChooserDialog("Save Database As...", parent, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+	# Run the dialog object, catching responses
 	response = dialog.run()
+	# Process the response
 	if response == Gtk.ResponseType.OK:
-		print("File selected: " + dialog.get_filename())
+		print("File selected: " + dialog.get_filename()) # debug code
+		# If the file is not correct, an error will occur, so catch it
 		try:
-			response = setFile(dialog.get_filename())
+			if db == None:
+				response = newDB(newFile(dialog.get_filename()))
+			else:
+				db.commit()
+				# TODO: work out how I'm going to get save as working
+		# If it is an error, send None
 		except (TypeError, NameError):
 			response = None
-			print("This is an invalid file!")
+			print("This is an invalid file!") # debug code
+	# If cancel is pressed, send None
 	elif response == Gtk.ResponseType.CANCEL:
-		print("Cancel clicked")
+		print("Cancel clicked") # debug code
 		response = None
+	# Remember to get rid of the dialog box
 	dialog.destroy()
+	# Return the database object
 	return response
